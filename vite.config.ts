a90b53225b6067a0ef4e34a8d2ae0b34d7b54fcf
gummydaddy <yadavvaibhav136@gmail.com>
2026-02-18 01:17:03 +0530

    was getting error in react extension made required updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      // These match the importmap in index.html exactly to ensure the browser handles resolution
+      // Externalize React and React-DOM to use from CDN via importmap
       external: [
-        'recharts'
+        'react',
+        'react-dom',
+        'react-dom/client'
       ],
     },
   },
